562614eea8f330a8d7f0abe8142031d74d45ff3f
rjmlaird <rjmlaird@gmail.com>
2025-07-30 03:27:26 +0100

    ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,16 +18,17 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Allow unused vars starting with _
     'no-undef': 'error',
     'no-empty': 'warn',
-    'no-prototype-builtins': 'off',
-    'no-cond-assign': ['error', 'except-parens'],
-    'no-useless-escape': 'warn',
+    'no-prototype-builtins': 'off', // Disable to allow Object.prototype.hasOwnProperty calls
+    'no-cond-assign': ['error', 'except-parens'], // Allow assignments inside parentheses
+    'no-useless-escape': 'warn', // Warn on unnecessary escapes
   },
   overrides: [
     {
-      files: ['.eslintrc.js', 'tools/**/*.js', 'postcss.config.js'], // added postcss config
+      // Node-specific config files (ESLint config, tooling, postcss)
+      files: ['.eslintrc.js', 'tools/**/*.js', 'postcss.config.js'],
       env: {
         node: true,
         browser: false,
@@ -39,13 +40,20 @@ module.exports = {
         __dirname: 'readonly',
       },
       rules: {
-        'no-undef': 'off',
+        'no-undef': 'off', // Node globals allowed, ignore no-undef here
       },
     },
     {
-      files: ['assets/js/prism.js', 'assets/js/mkdirp-hugo-mod.js', 'assets/js/plantuml.js', 'assets/js/markmap.js', 'assets/js/drawio.js'], // known legacy or third-party scripts
+      // Legacy or third-party scripts with known issues
+      files: [
+        'assets/js/prism.js',
+        'assets/js/mkdirp-hugo-mod.js',
+        'assets/js/plantuml.js',
+        'assets/js/markmap.js',
+        'assets/js/drawio.js',
+      ],
       rules: {
-        'no-undef': 'off',      // suppress undefined globals for legacy scripts
+        'no-undef': 'off',      // Suppress undefined global errors
         'no-unused-vars': 'warn', 
         'no-empty': 'warn',
         'no-useless-escape': 'warn',
