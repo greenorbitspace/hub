99c99924c0ee1120b176dddca7f8cd170c14d2cd
rjmlaird <rjmlaird@gmail.com>
2025-07-30 03:10:24 +0100

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,28 @@
 module.exports = {
   env: {
-    browser: true,
-    node: true,
-    es2021: true,
+    browser: true,     // for browser globals like window, document, jQuery
+    node: true,        // for Node.js globals like module, require, process, __dirname
+    es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:@eslint/js'],
+  globals: {
+    jQuery: 'readonly',
+    bootstrap: 'readonly',
+    lunr: 'readonly',
+    WorkerGlobalScope: 'readonly',
+    deflate: 'readonly',
+    zip_deflate_end: 'readonly',
+    global: 'readonly'
+  },
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
-    // Add custom rules if needed
-  },
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-undef': 'error',
+    'no-empty': 'warn',
+    'no-prototype-builtins': 'off',
+    'no-cond-assign': ['error', 'except-parens'],
+    'no-useless-escape': 'warn'
+  }
 };
